1f89c96168ac4890aa23b1ed364663e7810addcd
Katherine Abela <katherineabela@mail.weber.edu>
2024-04-24 03:00:57 -0400

    final

--- src/main.js
+++ src/main.js
@@ -89,4 +89,41 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el))
 
-// import "bootstrap"
+// Hide Header on on scroll down
+
+// var didScroll
+// var lastScrollTop = 0
+// var delta = 5
+// var navbarHeight = $("#nav").outerHeight()
+
+// $(window).scroll(function (event) {
+//   didScroll = true
+// })
+
+// setInterval(function () {
+//   if (didScroll) {
+//     hasScrolled()
+//     didScroll = false
+//   }
+// }, 0)
+
+// function hasScrolled() {
+//   var st = $(this).scrollTop()
+
+//   // Make sure they scroll more than delta
+//   if (Math.abs(lastScrollTop - st) <= delta) return
+
+//   // If they scrolled down and are past the navbar, add class .nav-up.
+//   // This is necessary so you never see what is "behind" the navbar.
+//   if (st > lastScrollTop && st > navbarHeight) {
+//     // Scroll Down
+//     $("#nav").css("top", -navbarHeight)
+//   } else {
+//     // Scroll Up
+//     if (st + $(window).height() < $(document).height()) {
+//       $("#nav").css("top", "0px")
+//     }
+//   }
+
+//   lastScrollTop = st
+// }
